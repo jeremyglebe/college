b62b0a445d97179da560a449fd2b0983d7f1a5b9
Jeremy Glebe <jeremyglebe@gmail.com>
2018-11-06 08:54:19 -0600

    Started matrix project

---
+++ matrix_project/matrices.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<random>
+using namespace std;
+
+//Definitions
+typedef float Matrix[10000][10000];
+void* matrix_processing(void*);
+
+//Global variables
+random_device rnd;
+Matrix A;
+Matrix B;
+
+int main(){
+
+    for(int r = 0; r < 10000; r++){
+    
+        for(int c = 0; c < 10000; c++){
+
+	    
+
+        }
+
+    }
+}
