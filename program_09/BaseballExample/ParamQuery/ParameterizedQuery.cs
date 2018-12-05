3070263e1d6416a304c02c80eb67db95ab4ed08a
Jeremy Glebe <jeremyglebe@gmail.com>
2018-12-05 13:55:54 -0600

    Finished program 09

---
+++ program_09/BaseballExample/ParamQuery/ParameterizedQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParamQuery
+{
+    public partial class ParameterizedQuery : Form
+    {
+        public ParameterizedQuery()
+        {
+            InitializeComponent();
+        }
+
+        private void playersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.playersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.baseballDataSet);
+
+        }
+
+        private void ParameterizedQuery_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'baseballDataSet.Players' table. You can move, or remove it, as needed.
+            this.playersTableAdapter.Fill(this.baseballDataSet.Players);
+
+        }
+
+        private void QueryFindButton_Click(object sender, EventArgs e)
+        {
+            playersTableAdapter
+                .FillByLastName(baseballDataSet.Players, QueryTextBox.Text);
+        }
+
+        private void BrowseAllButton_Click(object sender, EventArgs e)
+        {
+            playersTableAdapter.Fill(baseballDataSet.Players);
+            QueryTextBox.Text = "";
+        }
+    }
+}
