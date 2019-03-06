48371468803f6ad66b401046915d3e0c6c89d459
Jeremy Glebe <jeremyglebe@gmail.com>
2019-03-06 04:03:17 -0600

    more

--- Assignments/A05/effect_ascii_art.py
+++ Assignments/A05/effect_ascii_art.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 def ascii_art(filepath):
     # Open the file
@@ -18,3 +18,28 @@ def ascii_art(filepath):
             # Write the correct character (0:255) / 23 -> (0:11)
             text_file.write(ascii_chars[data[w * r + c] // 23])
         text_file.write('\n')
+
+def ascii_image(filepath):
+    # Open the file
+    base = Image.open(filepath).convert('L')
+    # Image width and height
+    w, h = base.size
+    # Create a new, blank white image to use for the ascii image
+    txt = Image.new('RGB', (w*10, h*10), (255,255,255))
+    # We need a font to print the characters in
+    fnt = ImageFont.truetype("arial.ttf", 18)
+    # We need an instance of imagedraw to draw with
+    d = ImageDraw.Draw(txt)
+
+    # Image data as a single grayscale brightness from 0-255
+    data = list(base.getdata())
+    # Characters used to draw an image (12 chars means index 0-11)
+    ascii_chars = [ '#', 'A', '@', '%', 'S', '+', '<', '*', ':', ',', '.', ' ']
+    for r in range(h):
+        for c in range(w):
+            # Write the correct character (0:255) / 23 -> (0:11)
+            d.text((c * 10, r * 10), ascii_chars[data[w * r + c] // 23], font=fnt, fill=(0,0,0))
+
+    txt.save(filepath.split('.')[0] + '.ascii_image.jpg', 'jpeg')
+
+ascii_image('apple.png')
