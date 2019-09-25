9ceb4fb3c1f2fa51938331ee42d67d060d4b8119
Jeremy Glebe <jeremyglebe@gmail.com>
2019-09-25 12:18:17 -0500

    reorganized code

--- programs/p02/dec_to_bin.py
+++ programs/p02/dec_to_bin.py
@@ -10,37 +10,21 @@ receives an invalid input.
 '''
 
 
-def _dtb(deci):
+def main():
     '''
-    Backend function used by the public dec_to_bin() function. Does the actual
-    value getting and binary string creation. (Recursively creates a string of
-    binary numbers from a decimal number.)
-    @param {int} deci: a decimal number >= -1
-    @return {string} binary representation of deci
+    Driver of the program. Loops until the user enters a '-1' or an invalid
+    input. Prints the binary strings for the inputs.
     '''
-    if deci > 0:
-        # get the correct bit for this iteration
-        bit = deci % 2
-        # adjust the number for the next iteration
-        deci = deci // 2
-        # call the next iteration
-        return _dtb(deci) + str(bit)
-    else:
-        # if we're at 0, return an empty string to append to
-        return ''
-
-
-def dec_to_bin(deci):
-    '''
-    Recursively creates a string of binary numbers from a decimal number. This
-    function is a front-end that calls _dtb() and then adjusts the output to
-    pad the binary string. (ensures there are at least 4 bits shown)
-    @param {int} deci: a decimal number >= -1
-    @return {string} binary representation of deci
-    '''
-    bstring = _dtb(deci)
-    # adjust string to be >= 4 chars, right justified, pad with 0's
-    return bstring.rjust(4, '0')
+    # Prompt the user
+    print("[Enter any integer > -1 to see it's binary representation]")
+    print("[Enter -1 to terminate the program]")
+    # get a decimal number from user input
+    num = get_num()
+    while num != -1:
+        # print the binary number version of the input
+        print(num, '=', dec_to_bin(num))
+        # get a decimal number input
+        num = get_num()
 
 
 def get_num():
@@ -67,21 +51,37 @@ def get_num():
         return -1
 
 
-def main():
+def dec_to_bin(deci):
     '''
-    Driver of the program. Loops until the user enters a '-1' or an invalid
-    input. Prints the binary strings for the inputs.
+    Recursively creates a string of binary numbers from a decimal number. This
+    function is a front-end that calls _dtb() and then adjusts the output to
+    pad the binary string. (ensures there are at least 4 bits shown)
+    @param {int} deci: a decimal number >= -1
+    @return {string} binary representation of deci
     '''
-    # Prompt the user
-    print("[Enter any integer > -1 to see it's binary representation]")
-    print("[Enter -1 to terminate the program]")
-    # get a decimal number from user input
-    num = get_num()
-    while num != -1:
-        # print the binary number version of the input
-        print(num, '=', dec_to_bin(num))
-        # get a decimal number input
-        num = get_num()
+    bstring = _dtb(deci)
+    # adjust string to be >= 4 chars, right justified, pad with 0's
+    return bstring.rjust(4, '0')
+
+
+def _dtb(deci):
+    '''
+    Backend function used by the public dec_to_bin() function. Does the actual
+    value getting and binary string creation. (Recursively creates a string of
+    binary numbers from a decimal number.)
+    @param {int} deci: a decimal number >= -1
+    @return {string} binary representation of deci
+    '''
+    if deci > 0:
+        # get the correct bit for this iteration
+        bit = deci % 2
+        # adjust the number for the next iteration
+        deci = deci // 2
+        # call the next iteration
+        return _dtb(deci) + str(bit)
+    else:
+        # if we're at 0, return an empty string to append to
+        return ''
 
 
 # if this file is executed directly, run main()
