2e2d6eeda547285aca6303db9b2328d4d38dd64d
Jeremy Glebe <jeremyglebe@gmail.com>
2019-10-09 11:35:44 -0500

    updated my beowulf programt to be 34% more awesome

--- programs/p03/beowulf.py
+++ programs/p03/beowulf.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 '''
 Beowulf Translation Program
 Author: Jeremy Glebe
@@ -11,79 +10,92 @@ top of the program.
 
 import re
 
-
-# Translation key to describe how to translate old English words in the program
-# Structure is as follows:
-# old word that should be replaced: {
-#     'replace': word that should replace the old word,
-#     'count': the counter that should be added to by replacements of this word
-# }
-# We list 'count' separately because in some cases (such as bairns and bairn),
-# both are instances of the root word 'bairn' but we need to replace them with
-# different modern words. We still want to count them together. So both use
-# 'bairn' as their 'count' value but have different 'replace' values.
 TRANSLATION_KEY = {
-    'bairns': {
-        'replace': 'children',
-        'count': 'bairn'
-    },
-    'bairn': {
-        'replace': 'child',
-        'count': 'bairn'
-    },
-    'bight': {
-        'replace': 'bay',
-        'count': 'bight'
-    },
-    'float': {
-        'replace': 'ship',
-        'count': 'float'
-    },
-    'carle': {
-        'replace': 'hero',
-        'count': 'carle'
-    }
+    'bairn': 'child',
+    'bight': 'bay',
+    'float': 'ship',
+    'carle': 'hero',
+}
+
+CORRECTION_KEY = {
+    'childs': 'children'
 }
 
 
 def main():
-    '''Driver of the program. Handles input, translation, and output.'''
+    '''Driver of the program. Handles input and output.'''
     with open('Beowulf.txt', encoding='utf8') as ifile, \
             open('Beowulf2.txt', 'w', encoding='utf8') as ofile:
-        # counting replacements made
-        rep_count = {}
         # read text from the file
         text = ifile.read()
-        # get only everything between "BEOWULF." and "ADDENDA."
-        # DOTALL makes the . character match with \n as well
+        # The story is between "BEOWULF." and "ADDENDA."
+        # findall returns a list, need the first (only) element
         text = re.findall(r'BEOWULF\..*ADDENDA\.', text, re.DOTALL)[0]
-        # replace text in the poem for each word in the translation key
-        for key, obj in TRANSLATION_KEY.items():
-            # get new text with replacement and number of replacements made
-            text, count = re.subn(
-                key, obj['replace'], text, flags=re.IGNORECASE)
-            # make sure that the key we're using is in the count object
-            if not obj['count'] in rep_count.keys():
-                rep_count[obj['count']] = 0
-            # increase the replacement count for the given word
-            rep_count[obj['count']] += count
-        # create a heading to print at the beginning of the program
-        head = 'Jeremy Glebe\n\n[Beowulf Translation Program] Translates a' \
-            + ' few old English words\nto more modern language in the UTF-8' \
-            + ' text file of "Beowulf". Counts\nthe number of replacements' \
-            + ' made.\n\n[Translation Key]\n'
-        for key, val in TRANSLATION_KEY.items():
-            head += '{0}: {1}\n'.format(key.rjust(10), val['replace'])
-        # write the heading to the output file
-        ofile.write(head)
+        # Translate and then correct the text
+        # counters keeps track of how many replacements have occured
+        text, counters = translate(text)
+        text = correct(text)
+        # write out general header stuff
+        ofile.write(header_string() + '\n')
+        ofile.write(translation_key_string() + '\n')
+        ofile.write(correction_key_string() + '\n')
         # write the number of replacements to the output file
-        ofile.write('\n[REPLACEMENTS]\n')
-        for key, num in rep_count.items():
-            ofile.write('Replaced ' + str(num) +
-                        ' instances of \'' + key + '\'.\n')
+        ofile.write('[REPLACEMENTS]\n')
+        for key, num in counters.items():
+            line = 'Replaced {0} instances of \'{1}\'.\n'.format(num, key)
+            ofile.write(line)
         # write the modified poem to the output file
         ofile.write('\n[TRANSLATED POEM]\n')
         ofile.write(text)
+
+
+def translate(string):
+    # counting replacements made
+    rep_count = {}
+    # replace text in the poem for each word in the translation key
+    for key, repl in TRANSLATION_KEY.items():
+        # get new text with replacement and number of replacements made
+        string, count = re.subn(key, repl, string, flags=re.IGNORECASE)
+        # make sure that the key we're using is in the count object
+        if not key in rep_count.keys():
+            rep_count[key] = 0
+        # increase the replacement count for the given word
+        rep_count[key] += count
+    # returns a tuple containing the translated string and a dictionary of
+    # counters for the number of translations made
+    return string, rep_count
+
+
+def correct(string):
+    # replace text in the poem for each word in the corrections key
+    for key, repl in CORRECTION_KEY.items():
+        # get new text with replacement
+        string = re.sub(key, repl, string, flags=re.IGNORECASE)
+        return string
+
+
+def header_string():
+    msg = '''\
+Jeremy Glebe\n
+[Beowulf Translation Program] Translates a few old English words
+to more modern language in the UTF-8 text file of "Beowulf". Counts
+the number of replacements made.\n\
+'''
+    return msg
+
+
+def translation_key_string():
+    msg = '[Translation Key]\n'
+    for key, val in TRANSLATION_KEY.items():
+        msg += '{0}: {1}\n'.format(key.rjust(10), val)
+    return msg
+
+
+def correction_key_string():
+    msg = '[Correction Key]\n'
+    for key, val in CORRECTION_KEY.items():
+        msg += '{0}: {1}\n'.format(key.rjust(10), val)
+    return msg
 
 
 if __name__ == '__main__':
