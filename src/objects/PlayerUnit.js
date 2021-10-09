9021d3a69fffc08f2fd10c5fc2ad530dee5ec736
Jeremy Glebe <jeremyglebe@gmail.com>
2021-10-09 13:10:27 -0500

    added movement function to player unit

--- src/objects/PlayerUnit.js
+++ src/objects/PlayerUnit.js
@@ -12,11 +12,29 @@ export class PlayerUnit extends Phaser.GameObjects.Sprite {
     constructor(scene, row, column, unit) {
         // Place player at the same coordinates as the hexagon they start on
         super(scene, scene.map.at(row, column).x, scene.map.at(row, column).y, unit.key);
+        // Store position in the hex map
+        this.row = row;
+        this.column = column;
+        // Additional, easier reference to map
+        this.map = scene.map;
         // Show the sprite over the tiles
         this.setDepth(UNIT_DEPTH);
         // Scale the characters b/c they are VERY small
         this.setScale(UNIT_SCALE);
         // Add this character to the scene once they are constructed
         this.scene.add.existing(this);
+    }
+
+    _move1(direction) {
+        let neighbors = this.map.at(this.row, this.column).adjacent();
+        let toRow = neighbors[direction].row;
+        let toCol = neighbors[direction].column;
+        let toHex = this.map.at(toRow, toCol);
+        return this.scene.tweens.add({
+            targets: [this],
+            x: toHex.x,
+            y: toHex.y,
+            duration: 500,
+        });
     }
 }
