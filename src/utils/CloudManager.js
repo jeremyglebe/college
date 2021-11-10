7190b560333a52f105a87164c88b613072f206e9
Jeremy Glebe <jeremyglebe@gmail.com>
2021-11-10 09:24:57 -0600

    removed global constants from cloud manager

--- src/utils/CloudManager.js
+++ src/utils/CloudManager.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider, User } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, User, signOut } from "firebase/auth";
+import { getFirestore, collection } from 'firebase/firestore';
 
 /** @type {import("@firebase/app").FirebaseAppSettings} */
 const firebaseConfig = {
@@ -10,10 +11,6 @@ const firebaseConfig = {
     messagingSenderId: "614875998821",
     appId: "1:614875998821:web:8e46822215a4e41128b48c"
 }
-/** @type {FirebaseApp} Create a firebase app connection */
-const APP = initializeApp(firebaseConfig);
-/** @type {GoogleAuthProvider} */
-const GOOGLE_AUTH = new GoogleAuthProvider();
 
 /**
  * Singleton manager for cloud-based operations (serverless multiplayer)
@@ -22,9 +19,18 @@ export class CloudManager extends Phaser.Events.EventEmitter {
     /** DO NOT CONSTRUCT THIS OBJECT DIRECTLY, USE CloudManager.get() */
     constructor() {
         super();
+        /** @type {FirebaseApp} Create a firebase app connection */
+        this.app = initializeApp(firebaseConfig);
+        /** @type {Auth} */
+        this.auth = getAuth(this.app);
+        /** @type {GoogleAuthProvider} */
+        this.authProvider = new GoogleAuthProvider();
+        /** @type {Firestore} The firestore instance (cloud database) */
+        this.db = getFirestore(this.app);
         /** @type {User|null} object of the currently logged in user */
         this.user = null;
     }
+    
     /**
      * @returns the single instance of the CloudManager, shared globally
      */
@@ -42,9 +48,8 @@ export class CloudManager extends Phaser.Events.EventEmitter {
      * @returns {User} The firebase user object of the authenticated user
      */
     async login() {
-        const auth = getAuth();
         try {
-            let result = await signInWithPopup(auth, GOOGLE_AUTH);
+            let result = await signInWithPopup(this.auth, this.authProvider);
             // The signed-in user info.
             this.user = result.user;
             console.log("Logged in with Google! ", this.user);
@@ -52,6 +57,31 @@ export class CloudManager extends Phaser.Events.EventEmitter {
         }
         catch (error) {
             console.log("Login Failed: ", error);
+        }
+    }
+
+    /**
+     * Signs a user out of their google account
+     */
+    async logout() {
+        await signOut(this.auth);
+        this.user = null;
+    }
+
+    /**
+     * Add a new game to the database hosted by the logged in user. Does
+     * nothing if no user is logged in.
+     */
+    async createGame() {
+        if (this.user) {
+            await this.games_collection.add({
+                host: this.user.username,
+                active: true,
+                lobby: true,
+                players: [
+                    this.user.username
+                ]
+            });
         }
     }
 
